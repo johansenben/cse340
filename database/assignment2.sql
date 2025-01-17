57e469dc906c201489527e5be65238603160b89f
Ben <ben@Bens-MacBook-Pro.local>
2025-01-16 19:53:25 -0700

    week02 team activity & assignment v1

---
+++ database/assignment2.sql
@@ -0,0 +1,31 @@
+--*************** Week 02 Assignment ***************
+--step 5-1:
+INSERT INTO public.account
+    (account_firstname, account_lastname, account_email, account_password)
+VALUES
+    ("Tony", "Stark", "tony@starkent.com", "Iam1ronM@n");
+--stem 5-2:
+UPDATE public.account
+SET account_type = "Admin"
+WHERE account_firstname = "Tony" AND account_lastname = "Stark";
+--step 5-3
+DELETE FROM public.account 
+WHERE account_firstname = "Tony" AND account_lastname = "Stark";
+--step 5-4
+UPDATE public.inventory 
+SET inv_description = REPLACE(inv_description, "small interiors", "a huge interior")
+WHERE inv_make = "GM" AND inv_model = "Hummer";
+--step 5-5
+SELECT make, model, "name"
+FROM public.inventory 
+INNER JOIN public.classification
+    ON public.classification.classification_id = public.inventory.classification_id
+WHERE classification_name = "Sport";
+--step 5-6
+UPDATE public.inventory 
+SET inv_image = REPLACE(inv_image, "/images", "/images/vehicles"),
+    inv_thumbnail = REPLACE(inv_thumbnail, "/images", "/images/vehicles");
+--step 5-7
+
+
+
